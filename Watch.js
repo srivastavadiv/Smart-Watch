73e98a233314bafe294f33377747beeb1114eaf4
srivastavadiv <84732846+srivastavadiv@users.noreply.github.com>
2021-10-14 20:12:06 +0530

    Add files via upload

---
+++ Watch.js
@@ -0,0 +1,22 @@
+
+
+
+var b=document.querySelector('#box1');
+var c=document.getElementById('social');
+var b1=document.querySelector('#box2');
+var b2=document.querySelector('#box3');
+
+b.addEventListener('mouseover', function(){
+	
+	c.src='https://i.imgur.com/Mplj1YR.png';
+});
+b1.addEventListener('mouseover', function(){
+	
+	c.src='https://i.imgur.com/PTgQlim.png';
+});
+b2.addEventListener('mouseover', function(){
+	
+	c.src='https://i.imgur.com/Zygu7I3.png';
+});
+
+			  
